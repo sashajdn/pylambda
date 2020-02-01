993a8ac311bf4aca4ebf9a065500f920f878a6c6
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-02-01 14:22:03 +0000

    Remove redundant conditions from (#3)

--- scheme/andco.py
+++ scheme/andco.py
@@ -1,4 +1,4 @@
-"""Recursive functions with collectors
+"""Recursive functions with collectors.
 
 NB: <>&co functions in little-schemer.
 """
@@ -22,21 +22,19 @@ def evens_only_and_co(l, col):
                                      multiply(car(l), p),
                                      s))
 
-        else:
-            return evens_only_and_co(cdr(l),
-                                     lambda evens, p, s:
-                                     col(evens,
-                                     p,
-                                     add(car(l), s)))
+        return evens_only_and_co(cdr(l),
+                                 lambda evens, p, s:
+                                 col(evens,
+                                 p,
+                                 add(car(l), s)))
 
-    else:
-        return evens_only_and_co(car(l),
-                                 lambda car_evens, carp, cars:
-                                 evens_only_and_co(cdr(l),
-                                                   lambda cdr_evens, cdrp, cdrs:
-                                                   col(cons(car_evens, cdr_evens),
-                                                       multiply(carp, cdrp),
-                                                       add(cars, cdrs))))
+    return evens_only_and_co(car(l),
+                             lambda car_evens, carp, cars:
+                             evens_only_and_co(cdr(l),
+                                               lambda cdr_evens, cdrp, cdrs:
+                                               col(cons(car_evens, cdr_evens),
+                                                   multiply(carp, cdrp),
+                                                   add(cars, cdrs))))
 
 
 def multiinsertLR_starred_and_co(new, oldL, oldR, l, col):
@@ -47,31 +45,31 @@ def multiinsertLR_starred_and_co(new, oldL, oldR, l, col):
     if is_null(l):
         return col(quote(), 0, 0)
 
-    elif is_atom(car(l)):
+    if is_atom(car(l)):
         if is_eq(car(l), oldL):
             return multiinsertLR_starred_and_co(new, oldL, oldR, cdr(l),
                                                 lambda newl, L, R:
                                                 col(cons(new, cons(oldL, newl)),
                                                     add1(L),
                                                     R))
-        elif is_eq(car(l), oldR):
+
+        if is_eq(car(l), oldR):
             return multiinsertLR_starred_and_co(new, oldL, oldR, cdr(l),
                                                 lambda newl, L, R:
                                                 col(cons(oldR, cons(new, newl)),
                                                     L,
                                                     add1(R)))
 
-        else:
-            return multiinsertLR_starred_and_co(new, oldL, oldR, cdr(l),
-                                                lambda newl, L, R:
-                                                col(cons(car(l), newl),
-                                                    L,
-                                                    R))
-    else:
-        return multiinsertLR_starred_and_co(new, oldL, oldR, car(l),
-                                            lambda carnl, carL, carR:
-                                            multiinsertLR_starred_and_co(new, oldL, oldR, cdr(l),
-                                                                         lambda cdrnl,  cdrL, cdrR:
-                                                                         col(cons(carnl, cdrnl),
-                                                                             add(carL, cdrL),
-                                                                             add(carR, cdrR))))
+        return multiinsertLR_starred_and_co(new, oldL, oldR, cdr(l),
+                                            lambda newl, L, R:
+                                            col(cons(car(l), newl),
+                                                L,
+                                                R))
+
+    return multiinsertLR_starred_and_co(new, oldL, oldR, car(l),
+                                        lambda carnl, carL, carR:
+                                        multiinsertLR_starred_and_co(new, oldL, oldR, cdr(l),
+                                                                     lambda cdrnl,  cdrL, cdrR:
+                                                                     col(cons(carnl, cdrnl),
+                                                                         add(carL, cdrL),
+                                                                         add(carR, cdrR))))
