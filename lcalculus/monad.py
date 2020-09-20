b01c023656d16923305bf1b365ce19f7e62975db
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-09-20 10:40:33 +0100

    Feat/monad expressions (#13)
    * Update README.md
    
    * add: monad expressions

--- lcalculus/monad.py
+++ lcalculus/monad.py
@@ -1,11 +1,19 @@
+import abc
 import functools
-from typing import Callable, TypeVar
+import operator as op
+from typing import Callable, TypeVar, Union
 
 
 T = TypeVar('T')
 
 
-class Number:
+class INode(abc.ABC):
+    @abc.abstractmethod
+    def __call__(self):
+        pass
+
+
+class Number(INode):
     def __init__(self, value: T):
         self.value = value
 
@@ -15,21 +23,79 @@ class Number:
     def __str__(self):
         return str(self.value)
 
+    def __call__(self):
+        return self
 
-class Operation:
+
+class Operation(INode):
     def __init__(self, operator: Callable[[T], T]):
         self.operator = operator
+
+    def __lshift__(self, other: Number):
+        return Number(self.operator(other.value))
+
+    def __rshift__(self, other: Number):
+        return Number(self.operator(other.value))
+
+    def __call__(self):
+        return self.operator
+
+
+class Expression(INode):
+    def __init__(self, exp):
+        self.exp = exp
+
+    def __call__(self):
+        return self.exp
+
+    def __lshift__(self, other):
+        return self.exp.__lshift__(other)
+
+    def __rshift__(self, other):
+        return self.exp.__rshift__(other)
+
+
+class Graph(INode):
+    def __init__(
+        self,
+        *,
+        operator: Callable[[T], T],
+        left: INode,
+        right: INode,
+    ):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+    def __call__(self):
+        return self.left() >> self.operator << self.right()
+
+    def __lshift__(self, other: Number):
+        return Number(self.operator(other.value))
 
     def __rshift__(self, other: Number):
         return Number(self.operator(other.value))
 
 
-# Example
+Add = Operation(op.add)  # type: ignore
+Mul = Operation(op.mul)  # type: ignore
 
-Add = Operation(lambda x, y: x + y)  # type: ignore
-Mul = Operation(lambda x, y: x * y)  # type: ignore
+e = Expression(
+    Number(4) >> Mul << Number(6)
+)
 
-# (10 + 5) * 2
-result = (Number(10) >> Add >> Number(5)) >> Mul >> Number(2)
+# operations or expressions
+graph = Graph(
+    operator=Add,  # type: ignore
+    left=Graph(  # type: ignore
+        operator=Mul,  # type: ignore
+        left=Number(4),
+        right=Number(10)
+    ),
+    right=Expression(
+        Number(6) >> Add << Number(10)
+    )
+)
 
-print(result)  # 30
+res = graph()
+print(res)  # 56
